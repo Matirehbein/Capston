0c697b7a771f01ff42743dfc9707f24839e147e1
tomas figueroa <tomiifiigueroa1993@gmail.com>
2025-09-29 04:06:17 -0300

    actualización de puerto para CRUD

--- Aurora/Api-Aurora/app.py
+++ Aurora/Api-Aurora/app.py
@@ -423,4 +423,4 @@ def detalle_sucursal(id_sucursal):
 # RUN
 # ===========================
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="127.0.0.1", port=8000, debug=True)
